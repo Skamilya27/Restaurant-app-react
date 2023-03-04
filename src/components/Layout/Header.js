7ae8f14dbce8ab5fc70bfff480393d6bcbe52d64
Suman <sumankamilya27@gmail.com>
2023-03-04 12:49:43 +0530

    header cmp

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,18 +1,19 @@
 import React, { Fragment } from 'react'
 import classes from './Header.module.css'
+import HeaderCartButton from './HeaderCartButton';
 
 import mealsImage from '../assets/meals.jpg'
 
 function Header(props) {
   return (
     <Fragment>
-        <header className={classes.header}>
-            <h1>Khana</h1>
-            <button>Cart</button>
-        </header>   
-        <div className={classes['main-image']}>
-            <img src={mealsImage} alt='food'/>
-        </div>
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <HeaderCartButton />
+      </header>
+      <div className={classes['main-image']}>
+        <img src={mealsImage} alt='A table full of delicious food!' />
+      </div>
     </Fragment>
   );
 }
