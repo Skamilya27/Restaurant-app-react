225e093b3760fc0f1a4be1777726022b35efe414
Suman <sumankamilya27@gmail.com>
2023-03-06 12:23:10 +0530

    added overlays

---
+++ src/components/UI/Backdrop.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import classes from './Modal.module.css'
+
+function Backdrop() {
+  return (
+    <div className={classes.backdrop}>
+      
+    </div>
+  )
+}
+
+export default Backdrop
